bceb7f705e2daa4237e7f260e8cf8d330326bf9c
Jairojuma <121519230+Jairojuma@users.noreply.github.com>
2023-07-11 12:43:44 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Nextjs and Node.js app
+ A Node.js and Nextjs software application integrated with Goodle APIs
---
+++ src/app/login/form.tsx
@@ -0,0 +1,75 @@
+"use client";
+
+import { signIn } from "next-auth/react";
+import { useSearchParams, useRouter } from "next/navigation";
+import { ChangeEvent, useState } from "react";
+
+export const LoginForm = () => {
+  const router = useRouter();
+  const [loading, setLoading] = useState(false);
+  const [formValues, setFormValues] = useState({
+    email: "",
+    password: "",
+  });
+  const [error, setError] = useState("");
+
+  const searchParams = useSearchParams();
+  const callbackUrl = searchParams.get("callbackUrl") || "/profile";
+
+  const onSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      setFormValues({ email: "", password: "" });
+
+      const res = await signIn("credentials", {
+        redirect: false,
+        email: formValues.email,
+        password: formValues.password,
+        callbackUrl,
+      });
+
+      setLoading(false);
+
+      console.log(res);
+      if (!res?.error) {
+        router.push(callbackUrl);
+      } else {
+        setError("invalid email or password");
+      }
+    } catch (error: any) {
+      setLoading(false);
+      setError(error);
+    }
+  };
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setFormValues({ ...formValues, [name]: value });
+  };
+
+  const input_style =
+    "form-control block w-full px-4 py-5 text-sm font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none";
+
+  return (
+    <form onSubmit={onSubmit}>
+      {error && (
+        <p className="text-center bg-red-300 py-4 mb-6 rounded">{error}</p>
+      )}
+      <a
+        className="px-7 py-2 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out w-full flex justify-center items-center mb-3"
+        style={{ backgroundColor: "#3b5998" }}
+        onClick={() => signIn("google", { callbackUrl })}
+        role="button"
+      >
+        <img
+          className="pr-2"
+          src="/images/google.svg"
+          alt=""
+          style={{ height: "2rem" }}
+        />
+        Continue with Google
+      </a>
+    </form>
+  );
+};
